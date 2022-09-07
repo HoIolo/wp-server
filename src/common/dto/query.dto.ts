82ec8ac738f9b3f79e321c8751c2839b66529609
智梓菌 <1805639380@qq.com>
2022-09-07 13:47:07 +0800

    feat: add class-validator

--- src/common/dto/query.dto.ts
+++ src/common/dto/query.dto.ts
@@ -1,5 +1,15 @@
+import { IsNumberString, IsOptional, IsString } from 'class-validator';
+
 export class QueryDTO {
+  @IsOptional()
+  @IsNumberString()
   readonly page: string | number;
+
+  @IsOptional()
+  @IsNumberString()
   readonly offset: string | number;
+
+  @IsOptional()
+  @IsString()
   readonly keyword: string;
 }
