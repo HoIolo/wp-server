7acc6c9ab26cef9ee525ec7daf7cd7a8e5bdcf55
1805639380 <1805639380@qq.com>
2023-10-05 00:51:57 +0800

    添加注释

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -21,6 +21,11 @@ import { PageDTO } from 'src/common/dto/page.dto';
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  /**
+   * 获取所有评论（分页）
+   * @param pageDto
+   * @returns
+   */
   @Get('comments')
   async getComments(@Query() pageDto: PageDTO) {
     const [rows, count] = await this.commentService.findAllByPage(pageDto);
@@ -31,6 +36,11 @@ export class CommentController {
     };
   }
 
+  /**
+   * 发表评论
+   * @param publishCommentDto
+   * @returns
+   */
   @Post('comment')
   async publishComment(@Body() publishCommentDto: PublishCommentDTO) {
     const result = await this.commentService.createComment(publishCommentDto);
@@ -49,6 +59,12 @@ export class CommentController {
     };
   }
 
+  /**
+   * 根据文章id获取评论（分页）
+   * @param articleId
+   * @param pageDto
+   * @returns
+   */
   @Get('/comment/:articleId')
   async getCommentByArticle(
     @Param('articleId', ParseIntPipe) articleId: number,
