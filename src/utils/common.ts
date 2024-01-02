f33832c10bc2e092f5649a19943f37a67b36d353
Hololo <1805639380@qq.com>
2024-01-02 11:37:05 +0800

    feat: 新增判空工具函数

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -8,3 +8,24 @@ export const handlePage = (pageDto: PageDTO) => {
   const skip = ((page as number) - 1) * Number(offset);
   return { skip, offset };
 };
+
+/**
+ *  判断是否为空
+ * @param value
+ * @returns
+ */
+export const isEmpty = (value: any): boolean => {
+  if (value === null || value === undefined) {
+    return true;
+  }
+
+  if (typeof value === 'string' || Array.isArray(value)) {
+    return value.length === 0;
+  }
+
+  if (typeof value === 'object') {
+    return Object.keys(value).length === 0;
+  }
+
+  return false;
+};
